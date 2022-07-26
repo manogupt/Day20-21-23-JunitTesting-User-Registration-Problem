3f3a5b8f2419fafcbfcf91024785cea84d8b492a
manogupt <manojgupta123929@gmail.com>
2022-07-26 17:56:50 +0530

    Added :Junit test to validate the user entry for first name,last name email id, phoneno, password

--- src/main/java/com/Bridgelabz/JunitTestingUserRegistrationProblem/App.java
+++
@@ -1,9 +0,0 @@
-package com.Bridgelabz.JunitTestingUserRegistrationProblem;
-
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Welcome to JunitTesting User Registration Problem" );
-    }
-}
