e71100a6694eccfff30eedf58843bb1e62525bdc
manogupt <manojgupta123929@gmail.com>
2022-07-26 17:50:02 +0530

    Added : welcome message

---
+++ src/main/java/com/Bridgelabz/JunitTestingUserRegistrationProblem/App.java
@@ -0,0 +1,9 @@
+package com.Bridgelabz.JunitTestingUserRegistrationProblem;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to JunitTesting User Registration Problem" );
+    }
+}
